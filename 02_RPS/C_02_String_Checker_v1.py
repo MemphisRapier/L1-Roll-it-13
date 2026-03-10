1a9ddb67d8e4eab1498b743638d1575e38be87e7
MemphisRapier <vukie4071@masseyhigh.school.nz>
2026-03-10 15:32:49 +1300

    Add files via upload

---
+++ 02_RPS/C_02_String_Checker_v1.py
@@ -0,0 +1,36 @@
+def string_checker(question, valid_ans):
+    """Checks users enter a valid answer based on a list.
+    Accepts first letter or full word"""
+
+    error = f"Please enter a valid response.  Choose {valid_ans}"
+
+    while True:
+        # Get user response and make sure it's lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # check if the user response is a word in the list
+            if item == user_response:
+                return item
+
+            #check if the user response is the same as
+            # the first letter of an item in the list
+            elif user_response == item[0]:
+                return item
+
+        #print error if user does not enter something that is valid
+        print(error)
+        print()
+
+
+
+# Main Routine
+
+# Valid answers for string checker
+yes_no = ("yes", "no")
+rps_valid_ans = ("rock", "paper", "scissors", "xxx")
+
+want_instructions = string_checker("Do you want to see the instructions?",
+                                   yes_no)
+
+print("You chose: ")
